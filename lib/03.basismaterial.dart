c2e2d8ae553f58fff96197cd804624d1ac4082f0
marcdc-tm <marc.decaluwe@thomasmore.be>
2020-12-29 14:54:00 +0100

    03.basismaterial aangepast

--- lib/03.basismaterial.dart
+++ lib/03.basismaterial.dart
@@ -8,13 +8,13 @@ class AppRoot extends StatelessWidget {
   Widget build(BuildContext buildContext) => MaterialApp(
     home: Scaffold(
       body: AppTree(),
-      appBar: AppBar(title: Text("Titel van de app"),),
+      appBar: AppBar(title: Text("Basiswidgets"),),
     ),
   );
 }
 
 class AppTree extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Text("Hier komt de inhoud van de app");
+    return Text("Hello world");
   }
 }
