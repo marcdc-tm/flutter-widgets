8d60a47f0837db0eaceb2755bef919f2db38b287
marcdc-tm <marc.decaluwe@thomasmore.be>
2020-12-29 14:42:20 +0100

    02.minimal.dart oplossing

--- lib/02.minimal.dart
+++ lib/02.minimal.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,11 +17,32 @@ class MijnStatefulWidget extends StatefulWidget {
 }
 
 class _MijnWidgetStaat extends State<MijnStatefulWidget> {
-  Color kleur = Colors.blue;
+  List<Color> kleuren = [Colors.blue, Colors.red, Colors.green, Colors.yellow, Colors.orange, Colors.brown, Colors.purple];
+  Random gen = Random();
+  Color kleur;
+  List<Color> gebruikteKleuren = [];
+
+  @override
+  void initState() {
+    kleur = kleuren[gen.nextInt(kleuren.length)];
+    gebruikteKleuren.add(kleur);
+    print(kleur);
+    super.initState();
+  }
 
   void veranderKleur() {
     setState(() {
-      kleur = kleur==Colors.blue ? Colors.red : Colors.blue;
+      List<Color> vrijeKleuren = [];
+      if (gebruikteKleuren.length<kleuren.length) {
+        for(Color k in kleuren) {
+          if (!gebruikteKleuren.contains(k)) vrijeKleuren.add(k);
+        }
+      } else {
+        vrijeKleuren = [...kleuren];
+        vrijeKleuren.remove(kleur);
+      }
+      kleur = vrijeKleuren[gen.nextInt(vrijeKleuren.length)];
+      gebruikteKleuren.add(kleur);
     });
     print(kleur);
   }
