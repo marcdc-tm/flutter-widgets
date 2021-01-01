c78a95b630b9bdf00648dee6e80c8be14b2f4027
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-01-01 16:10:37 +0100

    voorbeelden inheritedwidget aangepast

--- lib/20.gegevens-inheritedwidget.dart
+++ lib/20.gegevens-inheritedwidget.dart
@@ -1,57 +1,100 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(InheritContainer(child: AppRoot()));
+  runApp(AppRoot());
 }
 
 class AppRoot extends StatelessWidget {
   Widget build(BuildContext buildContext) => MaterialApp(
     home: Scaffold(
-      body: AppTree(),
+      body: KleurKiezer(child: TekstContainer()),
       appBar: AppBar(title: Text("Gegevens doorgeven: inherited widget"),),
     ),
   );
 }
 
-class AppTree extends StatefulWidget {
-  AppTreeState createState() => AppTreeState();
+class KleurKiezer extends StatefulWidget {
+  final Widget child;
+
+  KleurKiezer({this.child, Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => KleurKiezerState();
 }
 
-class AppTreeState extends State<AppTree> {
+class KleurKiezerState extends State<KleurKiezer> {
+  Color _kleur;
+
+  @override
   void initState() {
-    Timer(Duration(seconds: 3), () {
-      setState(() {
-        InheritContainer.of(context).kopTekst = TextStyle(
-          color: Color(0xffff0000),
-          fontSize: 40,
-          fontWeight: FontWeight.bold
-        );
-      });
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Text("Dis is de koptekst van de app", style: InheritContainer.of(context).kopTekst);
-  }
-}
-
-class InheritContainer extends InheritedWidget {
-  InheritContainer({Key key, Widget child}) : super(key: key, child: child);
-
-  TextStyle kopTekst = TextStyle(
-    color: Color(0xff4696ec),
-    fontSize: 40,
-    fontWeight: FontWeight.bold
-  );
-
-  static InheritContainer of(BuildContext context) {
-    return (context.dependOnInheritedWidgetOfExactType<InheritContainer>() as InheritContainer);
+    super.initState();
+    _kleur = Colors.red;
   }
 
   @override
-  bool updateShouldNotify(InheritedWidget oldWidget) {
-    return true;
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            kleurWidget("Rood", Colors.red),
+            kleurWidget("Groen", Colors.green),
+            kleurWidget("Blauw", Colors.blue),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        Expanded(
+          child: KleurGever(kleur: _kleur, child: widget.child),
+        )
+      ],
+    );
+  }
+
+  Widget kleurWidget(String kleurTekst, Color kleur) {
+    return Row(
+      children: <Widget>[
+        Radio(
+          value: kleur,
+          groupValue: _kleur,
+          onChanged: (value) {
+            setState(() {
+              _kleur = value;
+            });
+          },
+        ),
+        Text(
+          kleurTekst,
+          style: TextStyle(color: kleur),
+        ),
+      ],
+    );
+  }
+}
+
+class TekstContainer extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Dit is de tekst van de app", style: TextStyle(
+          color: KleurGever.of(context).kleur,
+          fontSize: 40,
+          fontWeight: FontWeight.bold
+      )),
+      margin: EdgeInsets.all(50),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+    );
+  }
+}
+
+class KleurGever extends InheritedWidget {
+  final Color kleur;
+
+  KleurGever({this.kleur, Widget child, Key key,}) : super(child: child, key: key,);
+
+  static KleurGever of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<KleurGever>() as KleurGever;
+
+  @override
+  bool updateShouldNotify(KleurGever oldWidget) {
+    return false;
   }
 }
