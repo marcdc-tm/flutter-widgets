53d212137ea826b8dee5aa4d22d8da71b46282a8
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-01-20 15:18:59 +0100

    10.permissions-oef finaal

--- lib/10.permissions-oef.dart
+++ lib/10.permissions-oef.dart
@@ -248,8 +248,9 @@ class _AppTreeState extends State<AppTree> {
     audioSpeler.play(track._file, isLocal: true);
     nrPlaying = index;
     Timer.periodic(Duration(seconds: 1), (mijnTimer) {
-      print(_position.inSeconds.toString() + ' : ' + _duration.inSeconds.toString());
-      if (_position.inSeconds==_duration.inSeconds) {
+      int verstreken = mijnTimer.tick;
+      print(verstreken.toString() + ' : ' +_position.inSeconds.toString() + ' : ' + _duration.inSeconds.toString());
+      if (_position.inSeconds==_duration.inSeconds || verstreken>_duration.inSeconds) {
         mijnTimer.cancel();
         setState(() {
           playNext();
